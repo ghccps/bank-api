6d2006d93339a819bb4c0b1aeb18119895ff5ce0
Fabio <flutzsoares12@gmail.com>
2024-07-22 19:00:14 -0300

    model

---
+++ src/main/java/org/fabio/bank_api/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.fabio.bank_api.domain.repository;
+
+import org.fabio.bank_api.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
